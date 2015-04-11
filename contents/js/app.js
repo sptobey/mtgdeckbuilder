ad4c6b72bb0b09ea8a06ee5657b8774ad89e498b
Haoran Fang <gougou9903@gmail.com>
2015-04-11 16:36:06 -0600

    instruction tab
    instruction videos

--- contents/js/app.js
+++ contents/js/app.js
@@ -7,3 +7,8 @@ $( "#deck" ).on( "click", function( event ) {
     $("#list").empty()
     deck.load()
 })
+
+$( "#instructions" ).on( "click", function( event ) {
+    $("#list").empty()
+    instructions.load()
+})
