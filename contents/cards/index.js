7a173e62e1e312779481eae06e6ec5f0f7b588ff
shatterspike1 <stupidinternet303@gmail.com>
2015-04-11 16:03:23 -0600

    UI works with index now; checkboxes give bools

--- contents/cards/index.js
+++ contents/cards/index.js
@@ -22,7 +22,7 @@ var cards = {
         return check[0].checked
     },
     
-    searchByName: function(name, white) {
+    searchByName: function(name, white, blue, black, red, green, colorless, cmc, type) {
         $.ajax({
             type:"GET",
             url: "https://api.deckbrew.com/mtg/cards?name=" + name,
@@ -30,8 +30,8 @@ var cards = {
                 if (data){
                     //console.log(data);
                         console.log(name)
-                        console.log(cards.onProcess(white))
-                        console.log("SearchData: ", data);
+                        console.log(cards.onProcess(white) , cards.onProcess(blue) , cards.onProcess(black))
+                    //    console.log("SearchData: ", data);
                         $.get("/mtgdeckbuilder/cards/view.jade", function(template) {
                         var html = jade.render(template, {
                             data: data
