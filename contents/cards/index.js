ca4f02fedbd63aac4bcc791b49b3e759c456306c
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-04-11 17:03:24 -0600

    query by color works

--- contents/cards/index.js
+++ contents/cards/index.js
@@ -22,7 +22,84 @@ var cards = {
         return check[0].checked
     },
     
-    searchByName: function(name, white, blue, black, red, green, colorless, cmc, type) {
+    searchByName: function(name, white, blue, black, red, green, type) {
+        
+        var query = "https://api.deckbrew.com/mtg/cards";
+        var isFirst = true;
+        
+        // name
+        if(name) {
+            if(isFirst) {
+                query = query + "?";
+                isFirst = false;
+            } else {
+                query = query + "&";
+            }
+            query = query + "name=" + name;
+        }
+        // white
+        if(cards.onProcess(white)) {
+            if(isFirst) {
+                query = query + "?";
+                isFirst = false;
+            } else {
+                query = query + "&";
+            }
+            query = query + "color=" + "white";
+        }
+        // blue
+        if(cards.onProcess(blue)) {
+            if(isFirst) {
+                query = query + "?";
+                isFirst = false;
+            } else {
+                query = query + "&";
+            }
+            query = query + "color=" + "blue";
+        }
+        // black
+        if(cards.onProcess(black)) {
+            if(isFirst) {
+                query = query + "?";
+                isFirst = false;
+            } else {
+                query = query + "&";
+            }
+            query = query + "color=" + "black";
+        }
+        // red
+        if(cards.onProcess(red)) {
+            if(isFirst) {
+                query = query + "?";
+                isFirst = false;
+            } else {
+                query = query + "&";
+            }
+            query = query + "color=" + "red";
+        }
+        // green
+        if(cards.onProcess(green)) {
+            if(isFirst) {
+                query = query + "?";
+                isFirst = false;
+            } else {
+                query = query + "&";
+            }
+            query = query + "color=" + "green";
+        }
+        if(type) {
+            if(isFirst) {
+                query = query + "?";
+                isFirst = false;
+            } else {
+                query = query + "&";
+            }
+            query = query + "type=" + type;
+        }
+        
+        console.log("cmc: ", cmc);
+        console.log("Query: ", query);
+        
         $.ajax({
             type:"GET",
             //"http://api.mtgdb.info/search/" + name
@@ -30,9 +107,9 @@ var cards = {
             success: function(data) {
                 if (data){
                     //console.log(data);
-                        console.log(name)
-                        console.log(cards.onProcess(white) , cards.onProcess(blue) , cards.onProcess(black))
-                    //    console.log("SearchData: ", data);
+                    console.log(name)
+                    console.log(cards.onProcess(white) , cards.onProcess(blue) , cards.onProcess(black))
+                    console.log("SearchData: ", data);
                         $.get("/mtgdeckbuilder/cards/view.jade", function(template) {
                         var html = jade.render(template, {
                             data: data
