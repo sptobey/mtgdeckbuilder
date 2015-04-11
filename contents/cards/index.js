a699e6d86c45ca7c60905db4af86b5041ad00014
shatterspike1 <stupidinternet303@gmail.com>
2015-04-11 15:54:01 -0600

    Fixed UI

--- contents/cards/index.js
+++ contents/cards/index.js
@@ -18,13 +18,19 @@ var cards = {
         });
     },
     
-    searchByName: function(name) {
+    onProcess: function(check){
+        return check[0].checked
+    },
+    
+    searchByName: function(name, white) {
         $.ajax({
             type:"GET",
             url: "https://api.deckbrew.com/mtg/cards?color=red&color=blue&rarity=rare&name=fire",
             success: function(data) {
                 if (data){
                     //console.log(data);
+                        console.log(name)
+                        console.log(cards.onProcess(white))
                         $.get("/mtgdeckbuilder/cards/view.jade", function(template) {
                         var html = jade.render(template, {
                             data: data
