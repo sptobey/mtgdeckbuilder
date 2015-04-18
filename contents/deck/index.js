e79cd531537f17d93f801a7a9cfd9fdd0867aff6
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-04-18 15:02:19 -0600

    Testing posting data locally

--- contents/deck/index.js
+++ contents/deck/index.js
@@ -5,8 +5,8 @@ var deck = {
             url: "https://api.mtgdb.info/cards/random",
             success: function(data) {
                 console.log("Data:", data);
-                if (data){
-                        $.get("/mtgdeckbuilder/deck/list.jade", function(template) {
+                if(data) {
+                    $.get("/mtgdeckbuilder/deck/list.jade", function(template) {
                         var html = jade.render(template, {
                             data: data
                         })
@@ -14,6 +14,30 @@ var deck = {
                     })
                 }
             }
+        });
+    },
+
+    postTest: function(event) {
+        //console.log("Event: ", event);
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/mtgdeckbuilder/storage/test.json.data",
+            data: {test:"testData"},
+            dataType: "json",
+            success: function(data) {
+                console.log("Post data: ", data);
+                if(data) {
+                    $.get("/mtgdeckbuilder/deck/list.jade", function(template) {
+                        var html = jade.render(template, {
+                            data: data
+                        })
+                        $("#list").html(html)
+                    })
+                }
+            }
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert(textStatus);
         });
     },
 
