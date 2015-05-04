d5cb4ad12cf0c48ba327a88c4651af8dfb62ffed
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-05-03 22:01:58 -0600

    build

---
+++ build/instructions/index.js
@@ -0,0 +1,48 @@
+var instructions = {
+
+    // listCards: function() {
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "https://api.deckbrew.com/mtg/cards?color=red&color=blue&rarity=rare&name=fire",
+    //         success: function(data) {
+    //             if (data){
+    //                 console.log("ExampleData: ", data);
+    //                     $.get("/mtgdeckbuilder/cards/list.jade", function(template) {
+    //                     var html = jade.render(template, {
+    //                         data: data
+    //                     })
+    //                     $("#list").html(html)
+    //                 })
+    //             }
+    //         }
+    //     });
+    // },
+    
+    // searchByName: function(name) {
+    //     $.ajax({
+    //         type:"GET",
+    //         url: "https://api.deckbrew.com/mtg/cards?name=" + name,
+    //         success: function(data) {
+    //             if (data){
+    //                 console.log("SearchData: ", data);
+    //                     $.get("/mtgdeckbuilder/cards/view.jade", function(template) {
+    //                     var html = jade.render(template, {
+    //                         data: data
+    //                     })
+    //                     $("#list").html(html)
+    //                 })
+    //             }
+    //         }
+    //     });
+    // },
+
+    load: function() {
+        $.get("/mtgdeckbuilder/instructions/ui.jade", function(template) {
+            var html = jade.render(template);
+            $("#searchdiv").html(html);
+        })
+        
+        // list cards
+        // cards.listCards();
+    }
+}
